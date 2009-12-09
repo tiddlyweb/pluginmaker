df9cbfb66e3326a1ac76e53dfd3c8f31f602349b
FND <fnd@oda.(none)>
2009-12-09 10:50:44 +0000

    minor adjustments to setup.py boilerplate

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-
 # YOU NEED TO EDIT THESE
 AUTHOR = 'Python Person'
 AUTHOR_EMAIL = 'python@example.org'
@@ -6,21 +5,23 @@ NAME = 'tiddlywebplugins.example'
 DESCRIPTION = 'The short description of my project'
 VERSION = '0.9'
 
+
 import os
+
 from setuptools import setup, find_packages
 
-# You should review the below so that it seems correct. install_requires
-# especially.
+
+# You should carefully review the below (install_requires especially).
 setup(
-        namespace_packages = ['tiddlywebplugins'],
-        name = NAME,
-        version = VERSION,
-        description = DESCRIPTION,
-        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
-        author = AUTHOR,
-        url = 'http://pypi.python.org/pypi/%s' % NAME,
-        packages = find_packages(exclude='test'),
-        author_email = AUTHOR_EMAIL,
-        platforms = 'Posix; MacOS X; Windows',
-        install_requires = ['setuptools', 'tiddlyweb'],
-        )
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    packages = find_packages(exclude='test'),
+    author_email = AUTHOR_EMAIL,
+    platforms = 'Posix; MacOS X; Windows',
+    install_requires = ['setuptools', 'tiddlyweb']
+    )
